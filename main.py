bbfb34542ebb0b8765c48b82110bc90e8b483be1
sh.mn.nat@gmail.com <naoyashop@yahoo.co.jp>
2015-07-19 20:44:27 +0900

    rm virtroom

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-#!/bin/python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -*- coding: cp932 -*-
 
+import sys
 import tweepy
+from tweepy import  Stream, TweepError
+import logging
+import urllib
+# import urllib2
+from datetime import datetime as dt
 
 
 
 
 
+
+def main():
+	# main program
+
+
+if __name__ == "__main__":
+  main()
